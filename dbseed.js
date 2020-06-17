b697498f289b6b65974dffedb5783c3f4c35e6d1
Florence Braddy <florencebraddy@hotmail.com>
2020-06-17 17:43:53 -0400

    initiate db connection

--- dbseed.js
+++ dbseed.js
@@ -16,3 +16,21 @@ const pool = sql.createPool({
     console.log(error);
   }
 })();
+
+(async function createUserTable() {
+  try {
+    const conn = await pool.getConnection();
+
+    conn.query("CREATE DATABASE IF NOT EXISTS foodblog");
+    conn.query("USER foodblog");
+
+    const userDb = await conn.query(
+      "CREATE TABLE IF NOT EXISTS user (username, VARCHAR(300) UNIQUE NOT NULL, profilepic VARCHAR(255), bio VARCHAR (4000), PRIMARY KEY(username) )"
+    );
+    console.log(userDb);
+
+    conn.release();
+  } catch (error) {
+    console.log(error);
+  }
+})();

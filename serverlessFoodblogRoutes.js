8d52ae8de027d2c21ab48ec0e5f9b8f1e599b056
Florence Braddy <florencebraddy@hotmail.com>
2020-06-22 17:49:07 -0400

    moved get all user info and single user to serverless

--- serverlessFoodblogRoutes.js
+++ serverlessFoodblogRoutes.js
@@ -262,6 +262,87 @@ app.put("/foodblogpost", authorizeUser, async (request, response) => {
   }
 });
 
+app.post("/foodblogpic", authorizeUser, async (request, response) => {
+  try {
+    console.log("POST food blog picture");
+    if (!request.body.username) {
+      response.status(400).send({ message: "This blogpic is missing a param" });
+    }
+    const conn = await pool.getConnection();
+    const queryResponse = await conn.execute(
+      `INSERT INTO foodblog.foodblogpic (s3uuid,description,foodblogpost) VALUES (?, ?, ?)`,
+      [
+        request.body.s3uuid,
+        request.body.description ? request.body.description : null,
+        request.body.foodblogpost
+      ]
+    );
+    conn.release();
+    console.log(queryResponse);
+    response.status(200).send({ message: queryResponse });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error });
+  }
+});
+
+app.get("/foodblogpics", authorizeUser, async (request, response) => {
+  try {
+    console.log("GET ALL food blog pics");
+    const conn = await pool.getConnection();
+    const recordSet = await conn.query(`SELECT * FROM foodblog.foodblogpic`);
+    conn.release();
+    console.log(recordSet);
+    response.status(200).send({ message: recordSet });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error });
+  }
+});
+
+app.get("/everything", authorizeUser, async (request, response) => {
+  try {
+    console.log("GET EVERYTHING FROM ALL TABLES");
+    const conn = await pool.getConnection();
+    const queryResponse = await conn.execute(
+      `SELECT * FROM foodblog.foodblogpic pics 
+          JOIN foodblog.foodblogpost posts
+          ON pics.foodblogpost = posts.id
+          JOIN foodblog.user users	
+          ON posts.username = users.username`
+    );
+    conn.release();
+    console.log(queryResponse[0]);
+    response.status(200).send({ message: queryResponse[0] });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error });
+  }
+});
+
+app.get("/everythingbyuser", authorizeUser, async (request, response) => {
+  try {
+    console.log("GET EVERYTHING FROM A USER");
+    const conn = await pool.getConnection();
+    const queryResponse = await conn.execute(
+      `SELECT * FROM foodblog.foodblogpic pics 
+          JOIN foodblog.foodblogpost posts
+          ON pics.foodblogpost = posts.id
+          JOIN foodblog.user users	
+          ON posts.username = users.username
+          WHERE users.username = ?`,
+
+      [request.query.username]
+    );
+    conn.release();
+    console.log(queryResponse[0]);
+    response.status(200).send({ message: queryResponse[0] });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error });
+  }
+});
+
 function authorizeUser(request, response, next) {
   next();
 }
